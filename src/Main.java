c1382fc2c7771114db74cdfbcd52731071fd0b1f
Nish <Nish@DESKTOP-MABDSAQ>
2022-12-30 17:47:10 +0530

    1st Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		String s = "Nishant Chendulkar";
+		String [] a = s.split("");
+		
+		String r = "";
+		String t = " ";
+		
+		
+	}
+
+}
